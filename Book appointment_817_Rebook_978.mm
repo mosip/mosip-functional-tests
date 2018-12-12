92bf08bbf0171f5646702a3fcac44ea9b326900d
nagas12 <43199476+nagas12@users.noreply.github.com>
2018-12-12 15:26:23 +0530

    mind map

---
+++ Book appointment_817_Rebook_978.mm
@@ -0,0 +1,40 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1543469898174" ID="ID_1987990592" MODIFIED="1543469914732" TEXT="Book appointment">
+<node COLOR="#339900" CREATED="1543469914732" ID="ID_941882176" LINK="https://mosipid.atlassian.net/browse/MOS-817" MODIFIED="1543905374669" POSITION="right" TEXT="MOS-817">
+<node COLOR="#339900" CREATED="1543469932731" HGAP="23" ID="ID_1544059849" MODIFIED="1543905378521" TEXT="Individual navigates to the &#x201c;Home Page&#x201d; post Login" VSHIFT="10">
+<node COLOR="#339900" CREATED="1543469975756" HGAP="18" ID="ID_672693615" MODIFIED="1543905384289" TEXT="Individual may opt to Book Appointment for Pre-Registration Application by clicking on Book Appointment during Create Pre-Registration" VSHIFT="-18">
+<node COLOR="#339900" CREATED="1543471721898" ID="ID_609750795" MODIFIED="1543905407904" TEXT="Individual will be redirected to Book Appointment page and then selects Registration center">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_609750795" ENDARROW="Default" ENDINCLINATION="633;0;" ID="Arrow_ID_1285440326" SOURCE="ID_1242860735" STARTARROW="None" STARTINCLINATION="633;0;"/>
+<node COLOR="#339900" CREATED="1543471880513" ID="ID_1095494410" MODIFIED="1543905423939" TEXT="System should provide the User with a default appointment selection: system to select Consecutively available Appointment spots">
+<node COLOR="#339900" CREATED="1543471905321" HGAP="16" ID="ID_1392038134" MODIFIED="1543905500667" TEXT="System should allow the User to select any of the Appointment Date available and any of the Appointment Spot available." VSHIFT="-14">
+<node COLOR="#339900" CREATED="1543471955084" HGAP="14" ID="ID_28856845" MODIFIED="1543905523160" TEXT="Individual may choose to continue to book appointment with the default appointment spots, else he can opt to select any of the spots of the preferred Registration center" VSHIFT="20">
+<node COLOR="#339900" CREATED="1543472818420" HGAP="19" ID="ID_964571217" MODIFIED="1543905527779" TEXT="If any Pre-Registration ID does not have Booking maped, system should notify User if he opts to continue without booking" VSHIFT="6">
+<node COLOR="#339900" CREATED="1543474293903" ID="ID_525493805" MODIFIED="1543905532394" TEXT="System should allow the user to click on Previous and redirect him to search Registration center">
+<node COLOR="#339900" CREATED="1543475393128" HGAP="18" ID="ID_1970898952" MODIFIED="1543905542121" TEXT="the appointment booking done should be removed and system should not save any previous selection" VSHIFT="20">
+<node COLOR="#339900" CREATED="1543481741999" HGAP="17" ID="ID_781730926" MODIFIED="1543905547484" TEXT="System should enable the Book if atleast one applicant is selected a Appointment Slot" VSHIFT="-8">
+<node COLOR="#339900" CREATED="1543481952028" HGAP="19" ID="ID_1761526144" MODIFIED="1543905555985" TEXT="IF only one applicant is present then he should be selected an Appointment slot."/>
+<node COLOR="#cc3300" CREATED="1543483622937" ID="ID_772011916" MODIFIED="1543905575501" TEXT="Booking disabled even after the booking is done"/>
+</node>
+<node COLOR="#cc3300" CREATED="1543483487171" ID="ID_1558586525" MODIFIED="1543905588952" TEXT="appointment booking done not removed">
+<node COLOR="#cc3300" CREATED="1543483546119" HGAP="21" ID="ID_373184712" MODIFIED="1543905588952" TEXT="System encounters an error" VSHIFT="9"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#339900" CREATED="1543470068483" ID="ID_1242860735" MODIFIED="1543922775192" TEXT="by selecting the Pre-Registration/Pre-Registrations and clicking on the Modify Appointment.">
+<arrowlink DESTINATION="ID_609750795" ENDARROW="Default" ENDINCLINATION="633;0;" ID="Arrow_ID_1285440326" STARTARROW="None" STARTINCLINATION="633;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1242860735" ENDARROW="Default" ENDINCLINATION="1166;0;" ID="Arrow_ID_1700423714" SOURCE="ID_829573162" STARTARROW="None" STARTINCLINATION="1166;0;"/>
+</node>
+</node>
+</node>
+<node CREATED="1543491044934" ID="ID_829573162" LINK="https://mosipid.atlassian.net/browse/MOS-978" MODIFIED="1543491208287" POSITION="left" TEXT="MOS-978">
+<arrowlink DESTINATION="ID_1242860735" ENDARROW="Default" ENDINCLINATION="1166;0;" ID="Arrow_ID_1700423714" STARTARROW="None" STARTINCLINATION="1166;0;"/>
+</node>
+</node>
+</map>
