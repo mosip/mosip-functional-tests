db326a9e0f312567b318a30dc82251ff6b4366c7
nagas12 <43199476+nagas12@users.noreply.github.com>
2018-12-12 15:29:34 +0530

    Add files via upload

---
+++ Cancel Appointment_MOS-818.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1543379839686" ID="ID_951903850" MODIFIED="1543379859234" TEXT="Cancel Appointment">
+<node COLOR="#339900" CREATED="1543467736383" ID="ID_1210100172" LINK="https://mosipid.atlassian.net/browse/MOS-818" MODIFIED="1543907142233" POSITION="right" TEXT="MOS-818">
+<node COLOR="#339900" CREATED="1543380004889" ID="ID_1177308792" MODIFIED="1543907147945" TEXT="Individual navigates to the &#x201c;Home Page&#x201d; post Login">
+<node COLOR="#339900" CREATED="1543380238544" ID="ID_1856271724" MODIFIED="1543907152124" TEXT="Individual may opt to cancel an Appointment for a Pre-Registration Application by clicking the delete icon" VSHIFT="10">
+<node COLOR="#339900" CREATED="1543382006430" HGAP="18" ID="ID_424443676" MODIFIED="1543922932799" TEXT="system should Cancel Appointment for the selected application only" VSHIFT="12">
+<node COLOR="#339900" CREATED="1543382032527" ID="ID_1397248691" MODIFIED="1543907171366" TEXT="System should not allow the User to cancel the Appointment if the appointment Booking is less than 48 hrs(configurable) from time of booking">
+<node COLOR="#339900" CREATED="1543383441466" HGAP="21" ID="ID_99124483" MODIFIED="1543907175633" TEXT="The individual is provided with two options: Discard entire application, Cancel Appointment and save details &gt; The applicant opts to choose &#x201c;Cancel Appointment and save details&#x201d;." VSHIFT="-16">
+<node COLOR="#339900" CREATED="1543399670086" ID="ID_3629108" MODIFIED="1543907179910" TEXT="on click of Delete icon the System should disable cancel Appointment if the Pre-Registration is in &apos;Pending Appointment&apos; status."/>
+<node COLOR="#cc3300" CREATED="1543466560574" ID="ID_689043767" MODIFIED="1543922986972" TEXT="Appointment for the selected application will be cancelled with message code : MOS-PAM-001">
+<node COLOR="#339900" CREATED="1543398756684" HGAP="16" ID="ID_196573016" MODIFIED="1543907202963" TEXT="System should unlock the time slot of the Pre-Registration on successful Appointment Cancellation" VSHIFT="9">
+<node COLOR="#339900" CREATED="1543466968744" ID="ID_515311975" MODIFIED="1543907206905" TEXT="Capture audit logs"/>
+</node>
+</node>
+</node>
+<node COLOR="#cc3300" CREATED="1543400913223" ID="ID_1980590644" MODIFIED="1543907230363" TEXT="System allowinguser cancel the appointment if the appointment is less than 48 hrs of booking">
+<node COLOR="#cc3300" CREATED="1543401114269" HGAP="29" ID="ID_35935884" MODIFIED="1543907224742" TEXT="System shows an error" VSHIFT="17"/>
+</node>
+</node>
+<node COLOR="#cc3300" CREATED="1543466795294" ID="ID_771528747" MODIFIED="1543907238574" TEXT="System cancels appointment for the unselected applications ">
+<node COLOR="#cc3300" CREATED="1543466912656" ID="ID_1323753150" MODIFIED="1543907244241" TEXT="System encounters error" VSHIFT="18"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ Registration center_MOS-816.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1542337244185" ID="ID_1347360097" MODIFIED="1542337255470" TEXT="Registration center">
+<node COLOR="#339900" CREATED="1542337267794" ID="ID_1663631270" MODIFIED="1542338039664" POSITION="right" TEXT="Individual navigates to the &#x201c;Home Page&#x201d; post Login">
+<node COLOR="#339900" CREATED="1542337299078" ID="ID_92685153" MODIFIED="1542338039664" TEXT="Individual will be redirected to Book Appointment page and then selects Registration center">
+<node COLOR="#339900" CREATED="1542337310568" ID="ID_483662780" MODIFIED="1542338039662" TEXT="Time selection page with calendar days along with number of spots available per calendar day will be displayed">
+<node COLOR="#339900" CREATED="1542337323066" ID="ID_73655545" MODIFIED="1542338039662" TEXT="Individual can select any of the calendar day which he wishes to visit Registration center">
+<node COLOR="#339900" CREATED="1542337385738" ID="ID_1883428347" MODIFIED="1542338039661" TEXT="System should display 7 calendar days(configurable) for the Individual to select">
+<node COLOR="#339900" CREATED="1542337427099" ID="ID_106970593" MODIFIED="1542338039661" TEXT="System should display Total number of spots per each calendar day">
+<node COLOR="#339900" CREATED="1542337446368" ID="ID_339094401" MODIFIED="1542338039661" TEXT="System should disable(Grey out) the Calendar day which is a Holiday for the Registration center selected">
+<node COLOR="#339900" CREATED="1542337494515" ID="ID_911739703" MODIFIED="1542338039661" TEXT="System should consider 8 hours(configurable) as working hours for Registration Center Selected">
+<node COLOR="#339900" CREATED="1542337335046" ID="ID_1506202854" MODIFIED="1543922699888" TEXT="Time slots of 15 minutes(configurable) each will be displayed">
+<node COLOR="#339900" CREATED="1542337351164" ID="ID_1607516693" MODIFIED="1542338039661" TEXT="Each time slot with Available spots will be displayed">
+<node COLOR="#cc0000" CREATED="1542337588838" ID="ID_1793510176" MODIFIED="1542338059858" TEXT="System should audit data (User ID, Timestamp, Registration center selected) using audit log manager"/>
+</node>
+</node>
+</node>
+<node COLOR="#cc0000" CREATED="1542337768712" ID="ID_206694401" MODIFIED="1542338059858" TEXT="User able to select the holiday date">
+<node COLOR="#cc0000" CREATED="1542337796479" HGAP="18" ID="ID_748486227" MODIFIED="1542338059857" TEXT="System shows an error message" VSHIFT="15"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#cc0000" CREATED="1542337698021" ID="ID_1671700671" MODIFIED="1542338320762" TEXT="Induvidual unable to select ">
+<node COLOR="#cc0000" CREATED="1542337726700" ID="ID_1812124439" MODIFIED="1542338320762" TEXT="System throws an exception"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
