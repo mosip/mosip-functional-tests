aa09eb89d2cb7a9b91f7cf9db07df762b039cf9d
M1049813 <M1049813@B2ML30358.mindtree.com>
2019-07-31 18:16:12 +0530

    Update in json precondtion code

--- automationtests/src/main/java/io/mosip/authentication/fw/precon/JsonPrecondtion.java
+++ automationtests/src/main/java/io/mosip/authentication/fw/precon/JsonPrecondtion.java
@@ -20,13 +20,19 @@ import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.XML;
 import org.testng.Reporter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.thoughtworks.xstream.XStream;
+
 import io.mosip.authentication.fw.util.FileUtil;
 import io.mosip.authentication.fw.util.AuthTestsUtil;
 import io.mosip.authentication.fw.util.RunConfigUtil;
@@ -361,9 +367,14 @@ public class JsonPrecondtion extends MessagePrecondtion{
 	 * 
 	 * @param Json content
 	 * @return String
+	 * @throws JsonProcessingException 
 	 */
-	public static String convertJsonContentToXml(String content) {
-		return XMLPREFIX + XMLROOT + ">" + org.json.XML.toString(new org.json.JSONObject(content)) + "</" + XMLROOT + ">";
+	public static String convertJsonContentToXml(String content) throws JsonProcessingException {
+		JSONObject jsonObj = new JSONObject(content);
+		Map<String, Object> jsonToMap = jsonToMap(jsonObj);
+		XmlMapper xmlMapper = new XmlMapper();
+		String xmlContent = xmlMapper.writeValueAsString(jsonToMap);
+		return XMLPREFIX + XMLROOT + ">" + xmlContent + "</" + XMLROOT + ">";
 	}
 
 	public static String parseAndReturnJsonContent(String inputContent,String inputValueToSet, String mapping) {
