b41bfea7ff17192ceab3d8e0901833b74eda35df
tabishkhan7 <tabish.khansfc@gmail.com>
2019-08-14 14:45:12 +0530

    MOS-28176 : Fixed Errors

--- automationtests/src/main/java/io/mosip/registrationProcessor/util/RegProcApiRequests.java
+++ automationtests/src/main/java/io/mosip/registrationProcessor/util/RegProcApiRequests.java
@@ -41,8 +41,15 @@ public class RegProcApiRequests extends BaseTestCase {
 
 		logger.info("REST:ASSURED:Sending a data packet to" + ApplnURI+url);
 		Cookie.Builder builder = new Cookie.Builder("Authorization", regProcAuthToken);
-		Response getResponse = given().cookie(builder.build()).relaxedHTTPSValidation().multiPart("file", file).expect().log().all().
-				when().post(ApplnURI+url).then().log().all().extract().response();
+		Response getResponse = null;
+		try {
+			 getResponse = given().cookie(builder.build()).relaxedHTTPSValidation().multiPart("file", file).expect().log().all().
+					when().post(ApplnURI+url).then().log().all().extract().response();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 		logger.info("REST:ASSURED: The response from request is:" + getResponse.asString());
 		logger.info("REST-ASSURED: the response time is: " + getResponse.time());
 		return getResponse;
