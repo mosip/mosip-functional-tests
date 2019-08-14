293742f89381c11758f00e475a92fa30508edd65
tabishkhan7 <tabish.khansfc@gmail.com>
2019-08-14 15:16:15 +0530

    Fixed Errors

--- automationtests/src/main/java/io/mosip/registrationProcessor/tests/PacketReceiver.java
+++ automationtests/src/main/java/io/mosip/registrationProcessor/tests/PacketReceiver.java
@@ -304,7 +304,7 @@ public class PacketReceiver extends  BaseTestCase implements ITest {
 			Verify.verify(setFinalStatus);
 			softAssert.assertAll();
 
-		} catch (IOException | ParseException e) {
+		} catch (Exception e) {
 			Assert.assertTrue(false, "not able to execute packet receiver method : "+ e.getCause());
 		}
 	}
