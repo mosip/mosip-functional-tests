01f8f5d12ebcf4a7951ee74006b3bea84f081aff
Ravikant-M1044571 <ravi.kant2@mindtree.com>
2019-08-21 18:19:26 +0530

    MOS-28434 MOS-28435: added script testdata for machineSpec Search and Filter

--- automationtests/src/main/java/io/mosip/admin/tests/FilterMachineSpec.java
+++ automationtests/src/main/java/io/mosip/admin/tests/FilterMachineSpec.java
@@ -158,7 +158,7 @@ public class FilterMachineSpec extends AdminTestUtil implements ITest {
 		String url=RunConfigUtil.objRunConfig.getAdminEndPointUrl() + RunConfigUtil.objRunConfig.getFilterMachineSpec();
 		logger.info("******Post request Json to EndPointUrl: " + url+
 				 " *******");
-		String cookieValue = getAuthorizationCookie(getCookieRequestFilePathForUinGenerator(),"https://"+System.getProperty("env.user")+".mosip.io/r2/v1/authmanager/authenticate/useridPwd",AUTHORIZATHION_COOKIENAME);
+		String cookieValue = getAuthorizationCookie(getCookieRequestFilePathForUinGenerator(),"https://"+System.getProperty("env.user")+".mosip.io/v1/authmanager/authenticate/useridPwd",AUTHORIZATHION_COOKIENAME);
 		postRequestAndGenerateOuputFileWithCookie(testCaseName.listFiles(), url, "request", "output-1-actual-response", 0, AUTHORIZATHION_COOKIENAME, cookieValue);
 		
 		Map<String, List<OutputValidationDto>> ouputValid = OutputValidationUtil.doOutputValidation(
