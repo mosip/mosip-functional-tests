a1bd5bdce2f2b298e0fa2037d483ced4467f71c8
Ravikant-M1044571 <ravi.kant2@mindtree.com>
2020-05-12 12:41:50 +0530

    MOSIP-1006 updated kernel test data for data changes

--- automationtests/src/main/java/io/mosip/kernel/tests/GetUsersBasedOnRegCenter.java
+++ automationtests/src/main/java/io/mosip/kernel/tests/GetUsersBasedOnRegCenter.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.testng.Assert;
 import org.testng.ITest;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -27,8 +30,10 @@ import org.testng.internal.TestResult;
 import com.google.common.base.Verify;
 
 import io.mosip.kernel.service.ApplicationLibrary;
+import io.mosip.kernel.service.AssertKernel;
 import io.mosip.kernel.util.CommonLibrary;
 import io.mosip.kernel.util.KernelAuthentication;
+import io.mosip.kernel.util.KernelDataBaseAccess;
 import io.mosip.kernel.util.TestCaseReader;
 import io.mosip.service.AssertResponses;
 import io.mosip.service.BaseTestCase;
@@ -55,7 +60,7 @@ public class GetUsersBasedOnRegCenter extends BaseTestCase implements ITest {
 	public CommonLibrary lib=new CommonLibrary();
 	private final Map<String, String> props = lib.readProperty("Kernel");
 	private final String getusersBasedOnRegCenter = props.get("getusersBasedOnRegCenter");
-	private JSONObject Expectedresponse = null;
+	private AssertKernel assertions = new AssertKernel();
 	private KernelAuthentication auth=new KernelAuthentication();
 
 	// Getting test case names and also auth cookie based on roles
@@ -90,30 +95,61 @@ public class GetUsersBasedOnRegCenter extends BaseTestCase implements ITest {
 		// getting request and expected response jsondata from json files.
 		JSONObject objectDataArray[] = new TestCaseReader().readRequestResponseJson(moduleName, apiName, testcaseName);
 		JSONObject actualRequest = objectDataArray[0];
-		Expectedresponse = objectDataArray[1];		
+		JSONObject expectedresponse = objectDataArray[1];		
 		// Calling the get method 
-		Response res=applicationLibrary.getWithPathParam(getusersBasedOnRegCenter, actualRequest,adminCookie);
+		Response response=applicationLibrary.getWithPathParam(getusersBasedOnRegCenter, actualRequest,adminCookie);
 		
 		//This method is for checking the authentication is pass or fail in rest services
-		new CommonLibrary().responseAuthValidation(res);
-		
-		// Removing of unstable attributes from response		
-		List<String> outerKeys = new ArrayList<String>();
-		List<String> innerKeys = new ArrayList<String>();
-		outerKeys.add("responsetime");
-		innerKeys.add("lastSyncTime");
-		if(testCaseName.equalsIgnoreCase("kernel_GetusersBasedOnRegCenter_smoke") || testCaseName.equals("kernel_GetusersBasedOnRegCenter_response_time"))
-		{
-			String password = res.getBody().jsonPath().get("response.userDetails[0].userPassword").toString();
-			String password1 = res.getBody().jsonPath().get("response.userDetails[1].userPassword").toString();
-			 ((JSONObject)((JSONArray)((JSONObject)Expectedresponse.get("response")).get("userDetails")).get(0)).put("userPassword", password).toString();
-			 ((JSONObject)((JSONArray)((JSONObject)Expectedresponse.get("response")).get("userDetails")).get(1)).put("userPassword", password1).toString();
-		}
-		// Comparing expected and actual response
-		logger.info(res.asString());
-		status = AssertResponses.assertResponses(res, Expectedresponse, outerKeys, innerKeys);
+				new CommonLibrary().responseAuthValidation(response);
+				if (testcaseName.toLowerCase().contains("smoke")) {
+
+					// fetching json object from response
+					JSONObject responseJson = (JSONObject) ((JSONObject) new JSONParser().parse(response.asString()))
+							.get("response");
+					if (responseJson == null || !responseJson.containsKey("userDetails"))
+						Assert.assertTrue(false, "Response does not contain userDetails");
+					String query = "select count(*) FROM master.reg_center_user where regcntr_id ='" + actualRequest.get("regid") + "' and is_active = true";
+					long obtainedObjectsCount = new KernelDataBaseAccess().validateDBCount(query, "masterdata");
+
+					// fetching json array of objects from response
+					JSONArray userDetailsFromGet = (JSONArray) responseJson.get("userDetails");
+					logger.info("===Dbcount===" + obtainedObjectsCount + "===Get-count===" + userDetailsFromGet.size());
+
+					// validating number of objects obtained form db and from get request
+					if (userDetailsFromGet.size() == obtainedObjectsCount) {
+
+						// list to validate existance of attributes in response objects
+						List<String> attributesToValidateExistance = new ArrayList<String>();
+						attributesToValidateExistance.add("userName");
+						attributesToValidateExistance.add("mail");
+						attributesToValidateExistance.add("userPassword");
+						attributesToValidateExistance.add("name");
+						attributesToValidateExistance.add("roles");
+
+						// key value of the attributes passed to fetch the data (should be same in all
+						// obtained objects)
+						HashMap<String, String> passedAttributesToFetch = new HashMap<String, String>();
+						
+
+						status = AssertKernel.validator(userDetailsFromGet, attributesToValidateExistance,
+								passedAttributesToFetch);
+					} else
+						{
+						status = false;
+						logger.info("Response from the request: "+response.asString());
+						}
+
+				}
+
+				else {
+					// add parameters to remove in response before comparison like time stamp
+					ArrayList<String> listOfElementToRemove = new ArrayList<String>();
+					listOfElementToRemove.add("responsetime");
+					status = assertions.assertKernel(response, expectedresponse, listOfElementToRemove);
+				}
+
 		if (!status) {
-			logger.info(res.asString());
+			logger.info(response.asString());
 		}
 		Verify.verify(status);
 		softAssert.assertAll();
