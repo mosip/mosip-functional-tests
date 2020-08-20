52ff67cfb8cc1a1028c9359ca8c000f76acbb731
Ravikant-M1044571 <ravi.kant2@mindtree.com>
2020-08-20 15:29:32 +0530

    updated kernel scripts to fix the failed cases

--- automationtests/src/main/java/io/mosip/kernel/tests/FetchBlackListedWord.java
+++ automationtests/src/main/java/io/mosip/kernel/tests/FetchBlackListedWord.java
@@ -6,12 +6,16 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.testng.Assert;
 import org.testng.ITest;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -32,6 +36,7 @@ import io.mosip.kernel.service.ApplicationLibrary;
 import io.mosip.kernel.service.AssertKernel;
 import io.mosip.kernel.util.CommonLibrary;
 import io.mosip.kernel.util.KernelAuthentication;
+import io.mosip.kernel.util.KernelDataBaseAccess;
 import io.mosip.kernel.util.TestCaseReader;
 import io.mosip.service.BaseTestCase;
 import io.restassured.response.Response;
@@ -93,11 +98,12 @@ public class FetchBlackListedWord extends BaseTestCase implements ITest {
 	 * 
 	 * @param fileName
 	 * @param object
+	 * @throws ParseException 
 	 * 
 	 */
 	@SuppressWarnings("unchecked")
 	@Test(dataProvider = "fetchData", alwaysRun = true)
-	public void fetchBlackListedWord(String testcaseName){
+	public void fetchBlackListedWord(String testcaseName) throws ParseException{
 		logger.info("Test Case Name:" + testcaseName);
 
 		// getting request and expected response jsondata from json files.
@@ -107,15 +113,57 @@ public class FetchBlackListedWord extends BaseTestCase implements ITest {
 		responseObject = objectDataArray[1];
 				response = applicationLibrary.getWithPathParam(FetchBlackListedWord_URI,objectData,adminCookie);
 		
-		// add parameters to remove in response before comparison like time stamp
-		ArrayList<String> listOfElementToRemove = new ArrayList<String>();
-		listOfElementToRemove.add("responsetime");
-		//This method is for checking the authentication is pass or fail in rest services
-		new CommonLibrary().responseAuthValidation(response);
-		status = assertions.assertKernel(response, responseObject, listOfElementToRemove);
-		if (!status) {
-			logger.debug(response);
-		}
+				//This method is for checking the authentication is pass or fail in rest services
+				new CommonLibrary().responseAuthValidation(response);
+				
+				if (testcaseName.toLowerCase().contains("smoke")) {
+					// fetching json object from response
+					JSONObject responseJson = (JSONObject) ((JSONObject) new JSONParser().parse(response.asString()))
+							.get("response");
+					if (responseJson == null || !responseJson.containsKey("blacklistedwords"))
+						Assert.assertTrue(false, "Response does not contain blacklistedwords");
+
+					String query = "select count(*) FROM master.blacklisted_words where is_active = true and lang_code = '"+ objectData.get("langcode") + "'";
+
+					long obtainedObjectsCount = new KernelDataBaseAccess().validateDBCount(query, "masterdata");
+
+					// fetching json array of objects from response
+					JSONArray dataFromGet = (JSONArray) responseJson.get("blacklistedwords");
+
+					logger.info("===Dbcount===" + obtainedObjectsCount + "===Get-count===" + dataFromGet.size());
+
+					// validating number of objects obtained form db and from get request
+					if (dataFromGet.size() == obtainedObjectsCount) {
+
+						// list to validate existance of attributes in response objects
+						List<String> attributesToValidateExistance = new ArrayList<String>();
+						attributesToValidateExistance.add("word");
+						attributesToValidateExistance.add("langCode");
+
+						// key value of the attributes passed to fetch the data (should be same in all
+						// obtained objects)
+						HashMap<String, String> passedAttributesToFetch = new HashMap<String, String>();
+						if (objectData != null) {
+							passedAttributesToFetch.put("langCode", objectData.get("langcode").toString());
+
+						}
+
+						status = AssertKernel.validator(dataFromGet, attributesToValidateExistance, passedAttributesToFetch);
+					} else 
+						status = false;
+						
+
+				}
+
+				else {
+					// add parameters to remove in response before comparison like time stamp
+					ArrayList<String> listOfElementToRemove = new ArrayList<String>();
+					listOfElementToRemove.add("responsetime");
+					status = assertions.assertKernel(response, responseObject, listOfElementToRemove);
+				}
+				if (!status) {
+					logger.info("response from request=="+response.asString());
+				}
 		Verify.verify(status);
 		softAssert.assertAll();
 	}
