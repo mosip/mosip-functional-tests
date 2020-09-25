b0efb2518277fe2c6952832dd61269b5be6c4c0e
Gaurav Sharan <gaurav.sharan@mindtree.com>
2020-09-25 17:48:11 +0530

    Commiting regproc latest code

---
+++ automationtests/src/main/java/io/mosip/registrationProcessor/regpacket/dto/DOBDto.java
@@ -0,0 +1,11 @@
+package io.mosip.registrationProcessor.regpacket.dto;
+
+import lombok.Data;
+
+@Data
+public class DOBDto {
+	
+	private String date;
+	private int age;
+
+}
--- automationtests/src/main/java/io/mosip/registrationProcessor/tests/SecurityTests.java
+++ automationtests/src/main/java/io/mosip/registrationProcessor/tests/SecurityTests.java
@@ -35,69 +35,72 @@ import io.mosip.service.BaseTestCase;
 import io.mosip.util.TokenGeneration;
 import io.restassured.response.Response;
 
-public class SecurityTests extends BaseTestCase implements ITest{
+public class SecurityTests extends BaseTestCase implements ITest {
 	protected static String testCaseName = "";
 
-	TokenGeneration generateToken=new TokenGeneration();
-	TokenGenerationEntity tokenEntity=new TokenGenerationEntity();
-	StageValidationMethods apiRequest=new StageValidationMethods();
-	Properties api=new Properties();
-	EncryptData encryptData=new EncryptData();
-	static String moduleName="RegProc"; 
-	static String apiName="SecurityTests";
-	RegistrationPacketSyncDTO registrationPacketSyncDto=null;
-	JSONObject requestToEncrypt=null;
-	JSONObject getRequest=null;
-	private final String encrypterURL="/v1/keymanager/encrypt";
-	File packet=null;
-	String regId="";
-	String centre_machine_refId="";
-	String validToken="";
-	String adminAuthToken="";
-	RegProcApiRequests apiRequests=new RegProcApiRequests();
-	RegProcDBCleanUp cleanUp=new RegProcDBCleanUp();
+	TokenGeneration generateToken = new TokenGeneration();
+	TokenGenerationEntity tokenEntity = new TokenGenerationEntity();
+	StageValidationMethods apiRequest = new StageValidationMethods();
+	Properties api = new Properties();
+	EncryptData encryptData = new EncryptData();
+	static String moduleName = "RegProc";
+	static String apiName = "SecurityTests";
+	RegistrationPacketSyncDTO registrationPacketSyncDto = null;
+	JSONObject requestToEncrypt = null;
+	JSONObject getRequest = null;
+	private final String encrypterURL = "/v1/keymanager/encrypt";
+	File packet = null;
+	String regId = "";
+	String centre_machine_refId = "";
+	String validToken = "";
+	String adminAuthToken = "";
+	RegProcApiRequests apiRequests = new RegProcApiRequests();
+	RegProcDBCleanUp cleanUp = new RegProcDBCleanUp();
+
 	public String getToken(String tokenType) {
-		String tokenGenerationProperties=generateToken.readPropertyFile(tokenType);
-		tokenEntity=generateToken.createTokenGeneratorDto(tokenGenerationProperties);
-		String token=generateToken.getToken(tokenEntity);
+		String tokenGenerationProperties = generateToken.readPropertyFile(tokenType);
+		tokenEntity = generateToken.createTokenGeneratorDto(tokenGenerationProperties);
+		String token = generateToken.getToken(tokenEntity);
 		return token;
-		}
+	}
+
 	@BeforeClass
 	public void getValidPacketPath() {
-		validToken=getToken("syncTokenGenerationFilePath");
-		adminAuthToken=getToken("getStatusTokenGenerationFilePath");
-		String propertyFilePath=apiRequests.getResourcePath()+"config/registrationProcessorAPI.properties";
-		FileReader apiReader=null;
+		validToken = getToken("syncTokenGenerationFilePath");
+		adminAuthToken = getToken("getStatusTokenGenerationFilePath");
+		String propertyFilePath = apiRequests.getResourcePath() + "config/registrationProcessorAPI.properties";
+		FileReader apiReader = null;
 		Properties folderPath = new Properties();
 		try {
-			apiReader=new FileReader(new File(propertyFilePath));
+			apiReader = new FileReader(new File(propertyFilePath));
 			api.load(apiReader);
-			FileReader reader=new FileReader(new File(apiRequests.getResourcePath()+"config/folderPaths.properties"));
+			FileReader reader = new FileReader(
+					new File(apiRequests.getResourcePath() + "config/folderPaths.properties"));
 			folderPath.load(reader);
 			reader.close();
 			apiReader.close();
-			} catch (IOException e1) {
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
- 		File file=new File(apiRequests.getResourcePath()+folderPath.getProperty("packetForSecurityTest"));
-		File[] listOfFiles=file.listFiles();
-		for(File f:listOfFiles) {
-			if(f.getName().contains(".zip")) {
-				packet=f;
+		File file = new File(apiRequests.getResourcePath() + folderPath.getProperty("packetForSecurityTest"));
+		File[] listOfFiles = file.listFiles();
+		for (File f : listOfFiles) {
+			if (f.getName().contains(".zip")) {
+				packet = f;
 				try {
-					registrationPacketSyncDto=encryptData.createSyncRequest(f,"NEW");
-					regId=registrationPacketSyncDto.getSyncRegistrationDTOs().get(0).getRegistrationId();
-					centre_machine_refId=regId.substring(0,5)+"_"+regId.substring(5, 10);
+					registrationPacketSyncDto = encryptData.createSyncRequest(f, "NEW");
+					regId = registrationPacketSyncDto.getSyncRegistrationDTOs().get(0).getRegistrationId();
+					centre_machine_refId = regId.substring(0, 5) + "_" + regId.substring(5, 10);
 				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				registrationPacketSyncDto.setRequesttime(apiRequests.getUTCTime().toString());
-				requestToEncrypt=encryptData.encryptData(registrationPacketSyncDto);
-			} else if(f.getName().equals("GetStatus")) {
-				for(File request:f.listFiles()) {
-					if(request.getName().toLowerCase().contains("request")) {
+				requestToEncrypt = encryptData.encryptData(registrationPacketSyncDto);
+			} else if (f.getName().equals("GetStatus")) {
+				for (File request : f.listFiles()) {
+					if (request.getName().toLowerCase().contains("request")) {
 						try {
 							getRequest = (JSONObject) new JSONParser().parse(new FileReader(request.getPath()));
 							getRequest.put("requesttime", apiRequests.getUTCTime().toString());
@@ -110,65 +113,76 @@ public class SecurityTests extends BaseTestCase implements ITest{
 			}
 		}
 	}
-	@Test(priority=1)
+
+	@Test(priority = 1)
 	public void syncRequestWithValidToken() {
-	
-	
-		Response res=apiRequests.postRequestToDecrypt(encrypterURL, requestToEncrypt, MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON, validToken);
+
+		Response res = apiRequests.postRequestToDecrypt(encrypterURL, requestToEncrypt, MediaType.APPLICATION_JSON,
+				MediaType.APPLICATION_JSON, validToken);
 		String encryptedData = res.jsonPath().get("response.data").toString();
-		LocalDateTime timeStamp=null;
+		LocalDateTime timeStamp = null;
 		try {
 			timeStamp = encryptData.getTime(regId);
-		} catch (ParseException | NullPointerException |IllegalArgumentException e) {
+		} catch (ParseException | NullPointerException | IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Response response=apiRequests.regProcSyncRequest(api.getProperty("syncListApi"), encryptedData, centre_machine_refId, timeStamp.toString()+"Z", MediaType.APPLICATION_JSON, validToken);
-		
-		Assert.assertTrue(response.jsonPath().get("errors[0].status").equals("FAILURE"));
+		Response response = apiRequests.regProcSyncRequest(api.getProperty("syncListApi"), encryptedData,
+				centre_machine_refId, timeStamp.toString() + "Z", MediaType.APPLICATION_JSON, validToken);
+		System.out.println(response.asString());
+		Assert.assertTrue(response.jsonPath().get("response[0].status").equals("SUCCESS"));
 	}
-	@Test (priority=2)
+
+	@Test(priority = 2)
 	public void syncRequestWithInvalidToken() {
-		validToken=getToken("syncTokenGenerationFilePath");
-		Response res=apiRequests.postRequestToDecrypt(encrypterURL, requestToEncrypt, MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON, validToken);
+		validToken = getToken("syncTokenGenerationFilePath");
+		Response res = apiRequests.postRequestToDecrypt(encrypterURL, requestToEncrypt, MediaType.APPLICATION_JSON,
+				MediaType.APPLICATION_JSON, validToken);
 		String encryptedData = res.jsonPath().get("response.data").toString();
-		LocalDateTime timeStamp=null;
+		LocalDateTime timeStamp = null;
 		try {
 			timeStamp = encryptData.getTime(regId);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Response response=apiRequests.regProcSyncRequest(api.getProperty("syncListApi"), encryptedData, centre_machine_refId, timeStamp.toString()+"Z", MediaType.APPLICATION_JSON, validToken+"ABC");
-		
+		Response response = apiRequests.regProcSyncRequest(api.getProperty("syncListApi"), encryptedData,
+				centre_machine_refId, timeStamp.toString() + "Z", MediaType.APPLICATION_JSON, validToken + "ABC");
+
 		Assert.assertTrue(response.jsonPath().get("errors[0].message").equals("Authentication Failed"));
 	}
-	@Test(priority=3)
+
+	@Test(priority = 3)
 	public void packetUploadWithValidToken() {
-	Response res=apiRequests.regProcPacketUpload(packet, api.getProperty("packetReceiverApi"), validToken);
-	//cleanUp.prepareQueryList(packet.getName().substring(0,packet.getName().lastIndexOf(".")));
-	Assert.assertTrue(res.statusCode()==200);
+		Response res = apiRequests.regProcPacketUpload(packet, api.getProperty("packetReceiverApi"), validToken);
+		// cleanUp.prepareQueryList(packet.getName().substring(0,packet.getName().lastIndexOf(".")));
+		Assert.assertTrue(res.statusCode() == 200);
 	}
-	@Test(priority=4)
+
+	@Test(priority = 4)
 	public void packetUploadWithInValidToken() {
-		Response res=apiRequests.regProcPacketUpload(packet, api.getProperty("packetReceiverApi"), validToken+"ABC");
-		
+		Response res = apiRequests.regProcPacketUpload(packet, api.getProperty("packetReceiverApi"),
+				validToken + "ABC");
+
 		Assert.assertTrue(res.jsonPath().get("errors[0].errorCode").equals("RPR-AUT-01"));
 	}
-	@Test(priority=5)
+
+	@Test(priority = 5)
 	public void packetStatusWithValidToken() {
-		adminAuthToken=getToken("getStatusTokenGenerationFilePath");
-		Response res=apiRequests.regProcPostRequest(api.getProperty("packetStatusApi"),getRequest, MediaType.APPLICATION_JSON,adminAuthToken);
+		adminAuthToken = getToken("getStatusTokenGenerationFilePath");
+		Response res = apiRequests.regProcPostRequest(api.getProperty("packetStatusApi"), getRequest,
+				MediaType.APPLICATION_JSON, adminAuthToken);
 		System.out.println(res.asString());
-		Assert.assertTrue(res.jsonPath().get("response[0].statusCode").equals("UPLOAD_PENDING"));
+		Assert.assertTrue(res.jsonPath().get("response[0].statusCode").equals("PROCESSED"));
 	}
-	@Test(priority=6)
+
+	@Test(priority = 6)
 	public void packetStatusWithInvalidToken() {
-		Response res=apiRequests.regProcPostRequest(api.getProperty("packetStatusApi"),getRequest,MediaType.APPLICATION_JSON, authToken);
+		Response res = apiRequests.regProcPostRequest(api.getProperty("packetStatusApi"), getRequest,
+				MediaType.APPLICATION_JSON, authToken);
 		System.out.println(res.asString());
 		Assert.assertTrue(res.jsonPath().get("errors[0].message").equals("Authentication Failed"));
 	}
-
 
 	/**
 	 * This method is used for generating report
@@ -177,7 +191,7 @@ public class SecurityTests extends BaseTestCase implements ITest{
 	 */
 	@AfterMethod(alwaysRun = true)
 	public void setResultTestName(ITestResult result) {
-		
+
 		Field method;
 		try {
 			method = TestResult.class.getDeclaredField("m_method");
@@ -185,13 +199,14 @@ public class SecurityTests extends BaseTestCase implements ITest{
 			method.set(result, result.getMethod().clone());
 			BaseTestMethod baseTestMethod = (BaseTestMethod) result.getMethod();
 			Field f = baseTestMethod.getClass().getSuperclass().getDeclaredField("m_methodName");
-			testCaseName =moduleName+"_"+apiName+"_"+ baseTestMethod.getMethodName();
+			testCaseName = moduleName + "_" + apiName + "_" + baseTestMethod.getMethodName();
 			f.setAccessible(true);
 			f.set(baseTestMethod, SecurityTests.testCaseName);
 		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-			logger.error("Exception occurred in Sync class in setResultTestName method "+e);
+			logger.error("Exception occurred in Sync class in setResultTestName method " + e);
 		}
 	}
+
 	@Override
 	public String getTestName() {
 		return this.testCaseName;
