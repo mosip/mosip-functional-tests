b22d75490b2ee504e0e13e852f7573a1159c8fb1
kiranrajbs <kiranraj.s@mindtree.com>
2021-05-03 14:58:40 +0530

    MOSIP-13621 URL's are updated as per 1.1.5 API changes

--- device-utility/mosip-device-reg/src/main/java/io/mosip/device/register/constants/Urls.java
+++ device-utility/mosip-device-reg/src/main/java/io/mosip/device/register/constants/Urls.java
@@ -2,13 +2,13 @@ package io.mosip.device.register.constants;
 
 public interface Urls {
 	public  static final String USER_AUTHENTICATE="/v1/authmanager/authenticate/useridPwd";
-	public  static final String CREATE_POLICY_GROUP="/partnermanagement/v1/policies/policies/policyGroup";
-	public  static final String GET_POLICY_GROUP="/partnermanagement/v1/pmpartners/pmpartners/policyname/";
-	public  static final String PARTNER_SELF_REGISTRATION="/partnermanagement/v1/partners/partners";
-	public  static final String RETREIVE_PARTNERS="/partnermanagement/v1/pmpartners/pmpartners";
-	public  static final String REGISTER_DEVICE="/partnermanagement/v1/partners/devicedetail";
-	public  static final String UPLOAD_CA_CERTIFICATE="/partnermanagement/v1/partners/partners/uploadCACertificate";
-	public  static final String UPLOAD_Partner_CERTIFICATE="/partnermanagement/v1/partners/partners/uploadPartnerCertificate";
-	public static final String APPROVE_BIOMETRICS="/partnermanagement/v1/partners/securebiometricinterface";
-	public static final String SIGNED_REGISTER_BIOMETRICS="/partnermanagement/v1/partners/registereddevices";
+	public  static final String CREATE_POLICY_GROUP="/v1/policymanager/policies/group/new";
+	public  static final String GET_POLICY_GROUP="/v1/policymanager/policies/group/";
+	public  static final String PARTNER_SELF_REGISTRATION="/v1/partnermanager/partners";
+	public  static final String RETREIVE_PARTNERS="/v1/partnermanager/partners?partnerType=";
+	public  static final String REGISTER_DEVICE="/v1/partnermanager/devicedetail";
+	public  static final String UPLOAD_CA_CERTIFICATE="/v1/partnermanager/partners/certificate/ca/upload";
+	public  static final String UPLOAD_Partner_CERTIFICATE="/v1/partnermanager/partners/certificate/upload";
+	public static final String APPROVE_BIOMETRICS="/v1/partnermanager/securebiometricinterface";
+	public static final String SIGNED_REGISTER_BIOMETRICS="/v1/partnermanager/registereddevices";
 }
