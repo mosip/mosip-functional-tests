4295894b5db33c6162388b148fd91880bd08cc09
Nandhukumar <nandhukumare@gmail.com>
2023-01-02 18:44:56 +0530

    add few more test cases

--- automationtests/src/main/java/io/mosip/admin/fw/util/AdminTestUtil.java
+++ automationtests/src/main/java/io/mosip/admin/fw/util/AdminTestUtil.java
@@ -172,6 +172,7 @@ public class AdminTestUtil extends BaseTestCase {
 	public static File oidcJWK1 = new File("src/main/resources/oidcJWK1.txt");
 	public static File oidcJWK2 = new File("src/main/resources/oidcJWK2.txt");
 	public static File clientPrivateKey = new File("src/main/resources/config/clientPrivateKey.txt");
+	public static final String XSRF_HEADERNAME = "X-XSRF-TOKEN";
 	//These variables are created to store IdP Cookie in a file and then use it for some apis
 	private static File IDPUINCookiesFile = new File("src/main/resources/IDPUINCookiesResponse.txt");
 	// public static String prevrReqTime=null;
@@ -200,6 +201,28 @@ public class AdminTestUtil extends BaseTestCase {
 		try {
 			response = RestClient.postRequestWithCookie(url, inputJson, MediaType.APPLICATION_JSON,
 					MediaType.APPLICATION_JSON, cookieName, token);
+			Reporter.log("<b><u>Actual Response Content: </u></b>(EndPointUrl: " + url + ") <pre>"
+					+ ReportUtil.getTextAreaJsonMsgHtml(response.asString()) + "</pre>");
+			return response;
+		} catch (Exception e) {
+			logger.error("Exception " + e);
+			return response;
+		}
+	}
+	
+	protected Response postRequestWithCookieAuthHeaderAndXsrfToken(String url, String jsonInput, String cookieName,
+			String role, String testCaseName) {
+		Response response = null;
+		HashMap<String, String> headers = new HashMap<String, String>();
+		headers.put(XSRF_HEADERNAME, props.getProperty("XSRFTOKEN"));
+		String inputJson = inputJsonKeyWordHandeler(jsonInput, testCaseName);
+		token = kernelAuthLib.getTokenByRole(role);
+//		token = headers + ";" + token;
+		logger.info("******Post request Json to EndPointUrl: " + url + " *******");
+		Reporter.log("<pre>" + ReportUtil.getTextAreaJsonMsgHtml(inputJson) + "</pre>");
+		try {
+			response = RestClient.postRequestWithMultipleHeadersAndCookies(url, inputJson, MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_JSON, cookieName, token, headers);
 			Reporter.log("<b><u>Actual Response Content: </u></b>(EndPointUrl: " + url + ") <pre>"
 					+ ReportUtil.getTextAreaJsonMsgHtml(response.asString()) + "</pre>");
 			return response;
