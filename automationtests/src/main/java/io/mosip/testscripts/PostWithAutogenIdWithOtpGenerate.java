c328a960bf28b51a67be17bb189e2f1dcabd0756
Nandhukumar <nandhukumare@gmail.com>
2023-07-04 11:53:09 +0530

    add wait time after VID generation

--- automationtests/src/main/java/io/mosip/testscripts/PostWithAutogenIdWithOtpGenerate.java
+++ automationtests/src/main/java/io/mosip/testscripts/PostWithAutogenIdWithOtpGenerate.java
@@ -192,9 +192,11 @@ public class PostWithAutogenIdWithOtpGenerate extends AdminTestUtil implements I
 			if ((!testCaseName.contains("ESignet_")) && (!testCaseName.contains("Resident_CheckAidStatus"))) {
 				logger.info("waiting for" + props.getProperty("Delaytime")
 						+ " mili secs after VID Generation In RESIDENT SERVICES");
+				Thread.sleep(Long.parseLong(props.getProperty("Delaytime")));
 			}
 		} catch (Exception e) {
 			logger.error("Exception : " + e.getMessage());
+			Thread.currentThread().interrupt();
 		}
 
 	}
