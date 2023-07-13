0d2683c0900cfd92bd9bcc0bde54a5ba784aa85b
Nandhukumar <nandhukumare@gmail.com>
2023-07-13 15:27:13 +0530

    MOSIP-28434

--- automationtests/src/main/java/io/mosip/testrunner/HealthChecker.java
+++ automationtests/src/main/java/io/mosip/testrunner/HealthChecker.java
@@ -15,6 +15,8 @@ import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
 import io.mosip.admin.fw.util.AdminTestUtil;
+import io.mosip.global.utils.GlobalConstants;
+import io.mosip.kernel.util.ConfigManager;
 import io.mosip.service.BaseTestCase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -22,17 +24,19 @@ import io.restassured.response.Response;
 public class HealthChecker implements Runnable {
 	private static final Logger logger = Logger.getLogger(HealthChecker.class);
 	public static boolean bTerminate = false;
-	public static String propsHealthCheckURL = MosipTestRunner.getResourcePath() + "/" + "config/healthCheckEndpoint.properties";
+	public static String propsHealthCheckURL = MosipTestRunner.getResourcePath() + "/"
+			+ "config/healthCheckEndpoint.properties";
 	public static boolean signalTerminateExecution = false;
-	public static Map<Object, Object> healthCheckFailureMapS = Collections.synchronizedMap(new HashMap<Object, Object>());
-	private String currentRunningModule = ""; 
-	
+	public static Map<Object, Object> healthCheckFailureMapS = Collections
+			.synchronizedMap(new HashMap<Object, Object>());
+	private String currentRunningModule = "";
+
 	public void setCurrentRunningModule(String currentModule) {
 		currentRunningModule = currentModule;
 	}
 
 	public void run() {
-		
+
 		File file = new File(propsHealthCheckURL);
 		FileReader fileReader = null;
 		List<String> controllerPaths = new ArrayList<String>();
@@ -41,15 +45,19 @@ public class HealthChecker implements Runnable {
 			fileReader = new FileReader(file);
 			bufferedReader = new BufferedReader(fileReader);
 			String line;
-		
+
 			while ((line = bufferedReader.readLine()) != null) {
-				if (line.trim().equals(""))
+				if (line.trim().equals("") || line.trim().startsWith("#"))
 					continue;
 				String[] parts = line.trim().split("=");
 				if (parts.length > 1) {
 					// only add health check required for the current running module
-					if (parts[0].contains(currentRunningModule)) 
+					if (parts[0].contains(currentRunningModule)) {
+						// If the actuator link contains esignet add to the list only if esignet is deployed
+						if (parts[1].contains(GlobalConstants.ESIGNET) && (!ConfigManager.IseSignetDeployed()))
+							continue;
 						controllerPaths.add(BaseTestCase.ApplnURI + parts[1]);
+					}
 				}
 			}
 		} catch (Exception e) {
@@ -64,12 +72,12 @@ public class HealthChecker implements Runnable {
 			boolean isAllServicesUp = true;
 			for (int i = 0; i < controllerPaths.size(); i++) {
 				String serviceStatus = checkActuatorNoAuth(controllerPaths.get(i));
-				
+
 				if (serviceStatus.equalsIgnoreCase("UP") == false) {
 					isAllServicesUp = false;
 					healthCheckFailureMapS.put(controllerPaths.get(i), serviceStatus);
 				}
-				
+
 			}
 			if (isAllServicesUp == false) {
 				signalTerminateExecution = true;
@@ -82,12 +90,12 @@ public class HealthChecker implements Runnable {
 			}
 		}
 	}
-	
+
 	public static String checkActuatorNoAuth(String actuatorURL) {
-		Response response =null;
+		Response response = null;
 		response = given().contentType(ContentType.JSON).get(actuatorURL);
-		if(response != null && 	response.getStatusCode() == 200 ) {
-			logger.info(response.getBody().asString());        	
+		if (response != null && response.getStatusCode() == 200) {
+			logger.info(response.getBody().asString());
 			JSONObject jsonResponse = new JSONObject(response.getBody().asString());
 			return jsonResponse.getString("status");
 		}
