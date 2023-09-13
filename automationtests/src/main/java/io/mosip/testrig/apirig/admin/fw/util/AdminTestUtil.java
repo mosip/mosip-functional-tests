b8c1e409301e202dbe98f3ad0ced3ee0ca8ce0d0
Nandhukumar <nandhukumare@gmail.com>
2023-09-13 21:48:54 +0530

    MOSIP-29380

--- automationtests/src/main/java/io/mosip/testrig/apirig/admin/fw/util/AdminTestUtil.java
+++ automationtests/src/main/java/io/mosip/testrig/apirig/admin/fw/util/AdminTestUtil.java
@@ -3169,6 +3169,18 @@ public class AdminTestUtil extends BaseTestCase {
 			jsonString = replaceKeywordWithValue(jsonString, "$UINCODEVERIFIERPOS1$", UIN_CODE_VERIFIER_POS_1);
 		}
 		
+		if (jsonString.contains("$CODECHALLENGE$")) {
+			jsonString = replaceKeywordWithValue(jsonString, "$CODECHALLENGE$", properties.getProperty("codeChallenge"));
+		}
+		
+		if (jsonString.contains("$CODEVERIFIER$")) {
+			jsonString = replaceKeywordWithValue(jsonString, "$CODEVERIFIER$", properties.getProperty("codeVerifier"));
+		}
+		
+		if (jsonString.contains("$VCICONTEXTURL$")) {
+			jsonString = replaceKeywordWithValue(jsonString, "$VCICONTEXTURL$", properties.getProperty("vciContextURL"));
+		}
+		
 		if (jsonString.contains("$PROOFJWT$")) {
 
 			String oidcJWKKeyString = getJWKKey(oidcJWK1);
