90a7eead1c3a4565cc3a2207a4b49ac3c2b4bf65
Nandhukumar <nandhukumare@gmail.com>
2023-11-14 12:29:03 +0530

    postgres sql password key change
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- automationtests/src/main/java/io/mosip/testrig/apirig/kernel/util/ConfigManager.java
+++ automationtests/src/main/java/io/mosip/testrig/apirig/kernel/util/ConfigManager.java
@@ -57,6 +57,10 @@ public class ConfigManager {
 	private static String MOSIP_AUTOMATION_CLIENT_SECRET = "mosip_testrig_client_secret";
 	private static String MOSIP_AUTOMATION_CLIENT_ID = "mosip_testrig_client_id";
 	private static String MOSIP_AUTOMATION_APP_ID = "mosip_automation_app_id";
+	
+	public static String DB_PASSWORD_KEY = "postgres-password";
+	public static String DB_USER_KEY = "db-su-user";
+	
 
 	private static String S3_HOST = "s3-host";
 	private static String S3_REGION = "s3-region";
@@ -80,24 +84,24 @@ public class ConfigManager {
 	private static String HIBERNATE_SHOW_SQL = "hibernate.show_sql";
 	private static String HIBERNATE_CONTEXT_CLASS = "hibernate.current_session_context_class";
 
-	private static String AUDIT_DB_USER = "db-su-user";
-	private static String AUDIT_DB_PASS = "postgresql-password";
+	private static String AUDIT_DB_USER = DB_USER_KEY;
+	private static String AUDIT_DB_PASS = DB_PASSWORD_KEY;
 	private static String AUDIT_DB_SCHEMA = "audit_db_schema";
 
-	private static String IDA_DB_USER = "db-su-user";
-	private static String IDA_DB_PASS = "postgresql-password";
+	private static String IDA_DB_USER = DB_USER_KEY;
+	private static String IDA_DB_PASS = DB_PASSWORD_KEY;
 	private static String IDA_DB_SCHEMA = "ida_db_schema";
 
-	private static String PMS_DB_USER = "db-su-user";
-	private static String PMS_DB_PASS = "postgresql-password";
+	private static String PMS_DB_USER = DB_USER_KEY;
+	private static String PMS_DB_PASS = DB_PASSWORD_KEY;
 	private static String PMS_DB_SCHEMA = "pms_db_schema";
 
-	private static String KM_DB_USER = "db-su-user";
-	private static String KM_DB_PASS = "postgresql-password";
+	private static String KM_DB_USER = DB_USER_KEY;
+	private static String KM_DB_PASS = DB_PASSWORD_KEY;
 	private static String KM_DB_SCHEMA = "km_db_schema";
 
-	private static String MASTER_DB_USER = "db-su-user";
-	private static String MASTER_DB_PASS = "postgresql-password";
+	private static String MASTER_DB_USER = DB_USER_KEY;
+	private static String MASTER_DB_PASS = DB_PASSWORD_KEY;
 	private static String MASTER_DB_SCHEMA = "master_db_schema";
 
 	private static String IAM_EXTERNAL_URL = "keycloak-external-url";
