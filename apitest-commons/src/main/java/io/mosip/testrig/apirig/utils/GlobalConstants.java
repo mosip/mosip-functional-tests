c58084183f6dabdf16c9c72eefc4506daa8e1f25
Jayesh Kharode <jayesh.kharode@technoforte.co.in>
2025-03-28 12:01:02 +0530

    MOSIP-40948
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- apitest-commons/src/main/java/io/mosip/testrig/apirig/utils/GlobalConstants.java
+++ apitest-commons/src/main/java/io/mosip/testrig/apirig/utils/GlobalConstants.java
@@ -6,6 +6,7 @@ public class GlobalConstants {
 	public static final String AUTH = "auth";
 	public static final String RESIDENTNEW = "residentNew";
 	public static final String MASTERDATA = "masterdata";
+	public static final String DSL = "dsl";
 	public static final String PREREGISTRATIONID = "preRegistrationId";
 	public static final String OPERATION = "operation";
 	public static final String USERNAME = "username";
