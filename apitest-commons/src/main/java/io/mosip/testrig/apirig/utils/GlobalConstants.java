e116fde38a5890cf3ac346715cb3202137f2be7d
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-06-25 09:59:36 +0530

    MOSIP-35628 Added utility method to check if CAPTCHA is enabled by fetching configuration from eSignet actuator
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- apitest-commons/src/main/java/io/mosip/testrig/apirig/utils/GlobalConstants.java
+++ apitest-commons/src/main/java/io/mosip/testrig/apirig/utils/GlobalConstants.java
@@ -251,4 +251,28 @@ public class GlobalConstants {
 	public static final String SESSION = "SESSION";
 	public static final String COOKIE_NAME = "cookieName";
 	public static final String ACCEPT_HEADER = "acceptHeader";
+	
+	public static final String ACTIVE_PROFILES = "activeProfiles";
+
+	public static final String ESIGNET_ACTUATOR_ENDPOINT_KEYWORD = "actuatorEsignetEndpoint";
+
+	public static final String ESIGNET_BASE_URL = ConfigManager.getEsignetBaseUrl();
+
+	public static final String ESIGNET_ACTUATOR_URL = ESIGNET_BASE_URL
+			+ ConfigManager.getproperty(ESIGNET_ACTUATOR_ENDPOINT_KEYWORD);
+
+	public static final String SYSTEM_ENV_SECTION = "systemEnvironment";
+
+    public static final String CLASS_PATH_APPLICATION_PROPERTIES = "classpath:/application.properties";
+
+	public static final String MOSIP_ESIGNET_CAPTCHA_REQUIRED = "mosip.esignet.captcha.required";
+
+	public static final String CLASS_PATH_APPLICATION_DEFAULT_PROPERTIES = "classpath:/application-default.properties";
+
+	public static final String DEFAULT_STRING = "default";
+
+	public static final String MOSIP_CONFIG_APPLICATION_HYPHEN_STRING = "mosip-config/application-";
+
+	public static final String DOT_PROPERTIES_STRING = ".properties";
+
 }
