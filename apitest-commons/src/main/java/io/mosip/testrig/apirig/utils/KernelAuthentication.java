b4971176a2fcdce44b9308e373a2a8a47233ad73
Prathmesh Jadhav <prathmesh.j@cyberpwn.com>
2026-03-06 14:28:37 +0530

    MOSIP-44098 - Move common JWT decode utilities from EsignetUtil to AdminTestUtil
    Signed-off-by: Prathmesh Jadhav <prathmesh.j@cyberpwn.com>

--- apitest-commons/src/main/java/io/mosip/testrig/apirig/utils/KernelAuthentication.java
+++ apitest-commons/src/main/java/io/mosip/testrig/apirig/utils/KernelAuthentication.java
@@ -76,6 +76,17 @@ public class KernelAuthentication extends BaseTestCase {
 	protected static final String ESIGNETUINCOOKIESRESPONSE = "ESignetUINCookiesResponse";
 	protected static final String ESIGNETVIDCOOKIESRESPONSE = "ESignetVIDCookiesResponse";
 	
+	private static final String TOKEN_URL = ConfigManager.getproperty("keycloak-external-url")
+			+ ConfigManager.getproperty("keycloakAuthTokenEndPoint");
+	private static final String GRANT_TYPE = "client_credentials";
+	private static final String CLIENT_ID = "client_id";
+	private static final String CLIENT_SECRET = "client_secret";
+	private static final String GRANT_TYPE_KEY = "grant_type";
+	private static final String ACCESS_TOKEN = "access_token";
+	
+    private static String partnerCookie = null;
+    private static String mobileAuthCookie = null;
+	
 	public static void setLogLevel() {
 		if (ConfigManager.IsDebugEnabled())
 			logger.setLevel(Level.ALL);
@@ -245,6 +256,55 @@ public class KernelAuthentication extends BaseTestCase {
 			return adminCookie;
 		}
 		
+	}
+
+	private static Response sendPostRequest(String url, Map<String, String> params) {
+		try {
+			return RestClient.postRequestWithFormDataBody(url, params);
+		} catch (Exception e) {
+			logger.error("Error sending POST request to URL: " + url, e);
+			return null;
+		}
+	}
+	
+    public static String getAuthTokenFromKeyCloak(String clientId, String clientSecret) {
+        Map<String, String> params = new HashMap<>();
+        params.put(CLIENT_ID, clientId);
+        params.put(CLIENT_SECRET, clientSecret);
+        params.put(GRANT_TYPE_KEY, GRANT_TYPE);
+
+        Response response = sendPostRequest(TOKEN_URL, params);
+
+        if (response == null) {
+            return "";
+        }
+        logger.info(response.getBody().asString());
+
+        org.json.JSONObject responseJson = new org.json.JSONObject(response.getBody().asString());
+        return responseJson.optString(ACCESS_TOKEN, "");
+    }
+	
+	public static String getAuthTokenByRole(String role) {
+		if (role == null)
+			return "";
+
+		String roleLowerCase = role.toLowerCase();
+		switch (roleLowerCase) {
+		case "partner":
+			if (!AdminTestUtil.isValidToken(partnerCookie)) {
+				partnerCookie = getAuthTokenFromKeyCloak(ConfigManager.getPmsClientId(),
+						ConfigManager.getPmsClientSecret());
+			}
+			return partnerCookie;
+		case "mobileauth":
+			if (!AdminTestUtil.isValidToken(mobileAuthCookie)) {
+				mobileAuthCookie = getAuthTokenFromKeyCloak(ConfigManager.getMPartnerMobileClientId(),
+						ConfigManager.getMPartnerMobileClientSecret());
+			}
+			return mobileAuthCookie;
+		default:
+			return "";
+		}
 	}
 
 	@SuppressWarnings("unchecked")
